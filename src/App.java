66170fb9a497dcdea0977223f461e268814a5c62
JanFranco <jfquinterop@ufpso.edu.co>
2026-03-28 08:10:04 -0500

    finalice el parcial

--- src/App.java
+++ src/App.java
@@ -1,5 +1,39 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner SC = new Scanner(System.in);
+    System.out.println("ingresar el nombre del estudiante");
+    String SC = "nombre";
+    System.out.println("Identificacion del estudiante");
+    String SC = "ID";
+    System.out.println("puntaje");
+    int SC = "puntaje";
+
+    For()
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     
+    
+
+
+    
+    
+    
     }
 }
